6eac7506ce632790ab6c29b6d58849c6c7a28304
Uriel Carneiro <urielhk17@gmail.com>
2021-12-03 14:19:11 -0300

    a

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   reactStrictMode: true,
+  async rewrites() {
+    return {
+      fallback: [
+        {
+          source: "/:path*",
+          destination: "https://home.nizen.com.br/:path*",
+        },
+      ],
+    };
+  },
 };
