623d3a2c34fddebe112273123fb38202f07a5120
Uriel Carneiro <urielhk17@gmail.com>
2021-12-03 14:48:25 -0300

    a

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
     return [
       {
         source: "/",
-        destination: "https://home.nizen.com.br",
+        destination: "https://checkout.nizen.com.br/checkout",
       },
     ];
   },
