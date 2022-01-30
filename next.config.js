0a44fe82fda1067a35b8ab2d64c473dd8d10531d
Uriel Carneiro <urielhk17@gmail.com>
2022-01-30 15:41:24 -0300

    a

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
     return {
       fallback: [
         {
-          source: "/",
+          source: "/checkout",
           destination: "https://checkout.nizen.com.br/checkout",
         },
         {
