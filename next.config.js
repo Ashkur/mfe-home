9973a39e122b31bea3b7bcc60cce7cd59ebce838
Uriel Carneiro <urielhk17@gmail.com>
2022-01-30 15:01:18 -0300

    comment nextconfig

--- next.config.js
+++ next.config.js
@@ -1,25 +1,25 @@
-module.exports = {
-  reactStrictMode: true,
-  async rewrites() {
-    return {
-      fallback: [
-        {
-          source: "/checkout",
-          destination: "https://checkout.nizen.com.br/checkout",
-        },
-        {
-          source: "/checkout/:path*",
-          destination: "https://checkout.nizen.com.br/checkout/:path*",
-        },
-        {
-          source: "/cra",
-          destination: "https://cra.nizen.com.br/cra",
-        },
-        {
-          source: "/cra/:path*",
-          destination: "https://cra.nizen.com.br/cra/:path*",
-        },
-      ],
-    };
-  },
-};
+// module.exports = {
+//   reactStrictMode: true,
+//   async rewrites() {
+//     return {
+//       fallback: [
+//         {
+//           source: "/checkout",
+//           destination: "https://checkout.nizen.com.br/checkout",
+//         },
+//         {
+//           source: "/checkout/:path*",
+//           destination: "https://checkout.nizen.com.br/checkout/:path*",
+//         },
+//         {
+//           source: "/cra",
+//           destination: "https://cra.nizen.com.br/cra",
+//         },
+//         {
+//           source: "/cra/:path*",
+//           destination: "https://cra.nizen.com.br/cra/:path*",
+//         },
+//       ],
+//     };
+//   },
+// };
