884e4fc5363e92a48dfe3a033cc5177eed946e7c
Uriel Carneiro <urielhk17@gmail.com>
2022-02-28 16:01:29 -0300

    remove old components

--- components/layout.js
+++ components/layout.js
@@ -1,10 +1,8 @@
 // COMPONENTS
-import { NavBar } from "@ashkur/teste.ui.nav-bar";
 
 export default function Layout({ children }) {
   return (
     <>
-      <NavBar />
       <main>{children}</main>
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import styles from "../styles/Home.module.css";
 
-import { Card } from "@ashkur/teste.ui.card";
+// import { Card } from "@ashkur/teste.ui.card";
 
 import Link from 'next/link'
 
@@ -12,11 +12,6 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <h1>MICRO-FRONTEND HOME 2</h1>
-      <Card
-        title="SHARED CARD"
-        description="Card shared with bit component"
-        imageUrl="https://images.unsplash.com/photo-1633113088942-99089f4abffa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
-      />
 
       <button onClick={saveToken}>save token</button>
 
