1b57a3028edefcca9bca5eaa156c69e10742f35e
Uriel Carneiro <urielhk17@gmail.com>
2022-05-10 14:08:53 -0300

    chore: log vercel env

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,8 @@ export default function Home() {
     localStorage.setItem('token', 'eyJhbGciOiJIUzI1NiJ9.eyJhdCI6IjIwMjItMDEtMzBUMTY6Mjc6MjIrMDA6MDAiLCJ1c2VyX2lkIjpudWxsLCJlbWFpbCI6InVyaWVsQGV2b2IuY29tLmJyIn0.Q9uwNDWuqwqhOA4VLdk9PhW0ePKspwHAHVtqpF8RouQ')
   }
 
+  console.log('vercel encvironment', process.env.VERCEL_ENV)
+
   return (
     <div className={styles.container}>
       <h1>MICRO-FRONTEND HOME 2</h1>
