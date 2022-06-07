69cc27511b84a37c918ea99f52fe6179312ab0ce
Uriel Carneiro <urielhk17@gmail.com>
2022-06-07 12:09:01 -0300

    redirect to checkout button

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
+import { useRouter } from 'next/router'
+
 import styles from "../styles/Home.module.css";
 
 import { Teste } from "@evob/evob.ui.teste/dist/teste";
 
-
 import Link from 'next/link'
 
 export default function Home() {
+  const router = useRouter()
+
+  const redirectToCheckout = () => {
+    router.push('/checkout')
+  }
+
   const saveToken = () => {
     localStorage.setItem('token', 'eyJhbGciOiJIUzI1NiJ9.eyJhdCI6IjIwMjItMDEtMzBUMTY6Mjc6MjIrMDA6MDAiLCJ1c2VyX2lkIjpudWxsLCJlbWFpbCI6InVyaWVsQGV2b2IuY29tLmJyIn0.Q9uwNDWuqwqhOA4VLdk9PhW0ePKspwHAHVtqpF8RouQ')
   }
@@ -17,6 +24,8 @@ export default function Home() {
       <h1>MICRO-FRONTEND HOME 2</h1>
 
       <button onClick={saveToken}>save token</button>
+
+      <button onClick={redirectToCheckout}>Redirect to checkout</button>
 
       <Link href="/cra">go to cra</Link>
       
